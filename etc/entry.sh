f0aa44af4409bfd41ee23c7f4f013c5f20ab8b72
Martin Wheeler <hello@martinwheeler.com.au>
2024-08-28 12:40:05 +1000

    fix: always download v+ mod

--- etc/entry.sh
+++ etc/entry.sh
@@ -11,14 +11,13 @@ else
 fi
 
 # We assume that if the valheim plus config is missing, that this is a fresh container
-if [ ! -f "${STEAMAPPDIR}/start_server_bepinex.sh" ]; then
-	# Are we in a valheim plus container?
-	if [ ! -z "$VALHEIM_PLUS_VERSION" ]; then
-		wget --max-redirect=30 -qO- https://github.com/Grantapher/ValheimPlus/releases/download/"${VALHEIM_PLUS_VERSION}"/UnixServer.tar.gz | tar xvzf - -C "${STEAMAPPDIR}"
-		chmod +x "${STEAMAPPDIR}/start_server_bepinex.sh"
-		cp "${STEAMAPPDIR}/start_server_bepinex.sh" "${STEAMAPPDIR}/copy_start_server_bepinex.sh"
-		sed "s/\"Dedicated\"/\$SERVER_WORLD_NAME/" "${STEAMAPPDIR}/copy_start_server_bepinex.sh" | sed "s/\"My server\"/\$SERVER_NAME/" | sed "s/\"secret\"/\$SERVER_PW/" > "${STEAMAPPDIR}/start_server_bepinex.sh"
-	fi
+# if [ ! -f "${STEAMAPPDIR}/start_server_bepinex.sh" ]; then
+# Are we in a valheim plus container?
+if [ ! -z "$VALHEIM_PLUS_VERSION" ]; then
+  wget --max-redirect=30 -qO- https://github.com/Grantapher/ValheimPlus/releases/download/"${VALHEIM_PLUS_VERSION}"/UnixServer.tar.gz | tar xvzf - -C "${STEAMAPPDIR}"
+  chmod +x "${STEAMAPPDIR}/start_server_bepinex.sh"
+  cp "${STEAMAPPDIR}/start_server_bepinex.sh" "${STEAMAPPDIR}/copy_start_server_bepinex.sh"
+  sed "s/\"Dedicated\"/\$SERVER_WORLD_NAME/" "${STEAMAPPDIR}/copy_start_server_bepinex.sh" | sed "s/\"My server\"/\$SERVER_NAME/" | sed "s/\"secret\"/\$SERVER_PW/" >"${STEAMAPPDIR}/start_server_bepinex.sh"
 fi
 
 cd "${STEAMAPPDIR}"
